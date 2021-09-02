508a96467af0ab848cd426f6d5d11002d5c33997
erikzon <ericksonoliva@gmail.com>
2021-09-02 17:19:33 -0600

    added font and current jobs

---
+++ app.js
@@ -0,0 +1,12 @@
+var en_US = {
+    welcome: "Hi! I'm"
+}
+
+var es_ES = {
+    welcome: "¡Hola! Soy"
+}
+
+var lang = es_ES;
+
+// set all the text
+document.getElementById("greeting").innerHTML = `${lang.welcome} Erick`;
