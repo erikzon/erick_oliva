ce1059c95b33c66104e67ed06bc3e9c35109849e
erikzon <ericksonoliva@gmail.com>
2021-09-02 21:55:56 -0600

    added language switcher

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-var en_US = {
-    welcome: "Hi! I'm"
+let en_US = {
+    greeting: "Hi! I'm",
+    whatiam: "I'm a Front-end Developer",
+    whatido: "Web developer on Javascript and PHP",
+    recentworks: "Recent Work"
 }
 
-var es_ES = {
-    welcome: "¡Hola! Soy"
+let es_ES = {
+    greeting: "¡Hola! Soy",
+    whatiam: "Soy Front-end Developer",
+    whatido: "Dessarollo aplicaciones web en Javacript y PHP",
+    recentworks: "Mis ultimos trabajos"
 }
 
-var lang = es_ES;
+let lang = es_ES;
 
-// set all the text
-document.getElementById("greeting").innerHTML = `${lang.welcome} Erick`;
+// set all the text at loadtime
+
+
+function changeLanguage(language) {
+    lang = language;
+    document.getElementById("recentworks").innerHTML = `${lang.recentworks}`;
+    document.getElementById("whatido").innerHTML = `${lang.whatido}`;
+    document.getElementById("whatiam").innerHTML = `${lang.whatiam}`;
+    document.getElementById("greeting").innerHTML = `${lang.greeting} Erick`;
+}
+
+changeLanguage(es_ES);
+
+//language selector nav logic
+document.getElementById("english").addEventListener("click", function () {
+    changeLanguage(en_US);
+});
+
+document.getElementById("spanish").addEventListener("click", function () {
+    changeLanguage(es_ES);
+});
