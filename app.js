20d697cfd5a0ecdd90d926c4dd7e9611ea01a53b
erikzon <ericksonoliva@gmail.com>
2021-10-07 20:57:11 -0600

    css refactor and responsive design for larger screens

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 let en_US = {
-    greeting: "Hi! Erick here",
-    whatiam: "I'm a Front-end Developer",
-    whatido: "Javascript and PHP Web developer, out from Guatemala.",
-    recentworks: "Recent Work",
-    aboutme: "About me",
-    myexperience: "I'm currently a computer science student at Universidad Mariano Galvez",
-    certificates: "Certificates",
+    greetingTitle: "Hi! Erick here",
+    whatiamTitle: "I'm a Front-end Developer",
+    whatidoParagraph: "Javascript and PHP Web developer, out from Guatemala.",
+    portfolioTitle: "Portfolio",
+    aboutmeTitle: "About me",
+    aboutmeParagraph: "I'm currently a computer science student at Universidad Mariano Galvez",
+    certificatesTitle: "Certificates",
     certificatescontent:"",
     skills: "Skills",
     skillscontent: "HTML5, CSS, JavaScript, Vue, Vuex, Vuetify, Tailwind, Bootstrap, Git"
 }
 
 let es_ES = {
-    greeting: "Erick Oliva",
-    whatiam: "Front-end Developer",
-    whatido: "Dessarollo aplicaciones web con tecnologias de Javacript y PHP, resido en Guatemala.",
-    recentworks: "Mis ultimos trabajos",
-    aboutme: "Acerca de mi",
-    myexperience: "Soy estudiante de 3er año en Ing. en Sistemas en la Universidad Mariano Galvez",
-    certificates: "Certificados",
+    greetingTitle: "Erick Oliva",
+    whatiamTitle: "Front-end Developer",
+    whatidoParagraph: "Dessarollo aplicaciones web con tecnologias de Javacript y PHP, resido en Guatemala.",
+    portfolioTitle: "Portafolio",
+    aboutmeTitle: "Acerca de mi",
+    aboutmeParagraph: "Soy estudiante de 3er año en Ing. en Sistemas en la Universidad Mariano Galvez",
+    certificatesTitle: "Certificados",
     certificatescontent:"",
     skills: "Habilidades",
     skillscontent: "HTML5, CSS, JavaScript, Vue, Vuex, Vuetify, Tailwind, Bootstrap, Git"
@@ -28,13 +28,13 @@ let lang = es_ES;
 
 function changeLanguage(language) {
     lang = language;
-    document.getElementById("recentworks").innerHTML = `${lang.recentworks}`;
-    document.getElementById("whatido").innerHTML = `${lang.whatido}`;
-    document.getElementById("whatiam").innerHTML = `${lang.whatiam}`;
-    document.getElementById("greeting").innerHTML = `${lang.greeting}`;
-    document.getElementById("aboutme").innerHTML = `${lang.aboutme}`;
-    document.getElementById("myexperience").innerHTML = `${lang.myexperience}`;
-    document.getElementById("certificates").innerHTML = `${lang.certificates}`;
+    document.getElementById("portfolioTitle").innerHTML = `${lang.portfolioTitle}`;
+    document.getElementById("whatidoParagraph").innerHTML = `${lang.whatidoParagraph}`;
+    document.getElementById("whatiamTitle").innerHTML = `${lang.whatiamTitle}`;
+    document.getElementById("greetingTitle").innerHTML = `${lang.greetingTitle}`;
+    document.getElementById("aboutmeTitle").innerHTML = `${lang.aboutmeTitle}`;
+    document.getElementById("aboutmeParagraph").innerHTML = `${lang.aboutmeParagraph}`;
+    document.getElementById("certificatesTitle").innerHTML = `${lang.certificatesTitle}`;
     //document.getElementById("certificatescontent").innerHTML = `${lang.certificatescontent}`;
     document.getElementById("skills").innerHTML = `${lang.skills}`;
     document.getElementById("skillscontent").innerHTML = `${lang.skillscontent}`;
