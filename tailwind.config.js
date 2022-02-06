c8fc7835c8ddf8021bd3d4f9370dfca614f9e1a3
erikzon <ericksonoliva@gmail.com>
2022-02-06 17:12:45 -0600

    added skills logo and mobile footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./public/index.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      const newUtilities = {
+        '.horizontal-tb': {
+          writingMode: 'horizontal-tb',
+        },
+        '.vertical-rl': {
+          writingMode: 'vertical-rl',      
+        },
+        '.vertical-lr': {
+          writingMode: 'vertical-lr'
+        }
+      }
+      addUtilities(newUtilities)
+    })
+  ],
 }
