61721c36b013757116a6da1296d22f008b550ca6
erikzon <ericksonoliva@gmail.com>
2022-02-10 14:52:27 -0600

    added script to changecolor depending on intersect

--- public/app.js
+++ public/app.js
@@ -1,3 +1,81 @@
+const sectionHome = document.getElementById('home');
+const sectionAboutMe = document.getElementById('aboutMe');
+const sectionSkills = document.getElementById('skills');
+const sectionMyPortfolio = document.getElementById('myPortfolio');
+
+const homeNavItem = document.getElementById('homeNavItem');
+const aboutMeNavItem = document.getElementById('aboutMeNavItem');
+const skillsNavItem = document.getElementById('skillsNavItem');
+const myportfolioNavItem = document.getElementById('myportfolioNavItem');
+
+const mobileHomeNavItem = document.getElementById('mobileHomeNavItem');
+const mobileAboutMeNavItem = document.getElementById('mobileAboutMeNavItem');
+const mobileSkillsNavItem = document.getElementById('mobileSkillsNavItem');
+const mobileMyportfolioNavItem = document.getElementById('mobileMyportfolioNavItem');
+
+const styleHomeNavItem = (entry, observador) => {
+    if (entry[0].isIntersecting) {
+        homeNavItem.classList.add('nav-item-active');
+        mobileHomeNavItem.classList.add('nav-item-active');
+    } else {
+        homeNavItem.classList.remove('nav-item-active');
+        mobileHomeNavItem.classList.remove('nav-item-active');
+    }
+}
+
+const styleAboutMeNavItem = (entry, observador) => {
+    if (entry[0].isIntersecting) {
+        aboutMeNavItem.classList.add('nav-item-active');
+        mobileAboutMeNavItem.classList.add('nav-item-active');
+    } else {
+        aboutMeNavItem.classList.remove('nav-item-active');
+        mobileAboutMeNavItem.classList.remove('nav-item-active');
+    }
+}
+
+const styleSkillsNavItem = (entry, observador) => {
+    if (entry[0].isIntersecting) {
+        skillsNavItem.classList.add('nav-item-active');
+        mobileSkillsNavItem.classList.add('nav-item-active');
+    } else {
+        skillsNavItem.classList.remove('nav-item-active');
+        mobileSkillsNavItem.classList.remove('nav-item-active');
+    }
+}
+
+const styleMyPortfolioNavItem = (entry, observador) => {
+    if (entry[0].isIntersecting) {
+        myportfolioNavItem.classList.add('nav-item-active');
+        mobileMyportfolioNavItem.classList.add('nav-item-active');
+    } else {
+        myportfolioNavItem.classList.remove('nav-item-active');
+        mobileMyportfolioNavItem.classList.remove('nav-item-active');
+    }
+}
+
+const observerHome = new IntersectionObserver(styleHomeNavItem, {
+    root: null,    rootMargin: '0px 0px 0px 0px',    threshold: 0.5
+});
+
+const observerAboutMe = new IntersectionObserver(styleAboutMeNavItem, {
+    root: null,    rootMargin: '0px 0px 0px 0px',    threshold: 0.5
+});
+
+const observerSkills = new IntersectionObserver(styleSkillsNavItem, {
+    root: null,    rootMargin: '0px 0px 0px 0px',    threshold: 0.5
+});
+
+const observerMyPortfolio = new IntersectionObserver(styleMyPortfolioNavItem, {
+    root: null,    rootMargin: '0px 0px 0px 0px',    threshold: 0.5
+});
+
+observerHome.observe(sectionHome);
+observerAboutMe.observe(sectionAboutMe);
+observerSkills.observe(sectionSkills);
+observerMyPortfolio.observe(sectionMyPortfolio);
+// observer.observe(sectionContactMe);
+
+/*
 let en_US = {
     greetingTitle: "Hi! Erick here",
     whatiamTitle: "I'm a Front-end Developer",
@@ -51,3 +129,4 @@ document.getElementById("english").addEventListener("click", function () {
 document.getElementById("spanish").addEventListener("click", function () {
     changeLanguage(es_ES);
 });
+*/
